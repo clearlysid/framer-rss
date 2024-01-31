47978d76270b18b7dfefb8bedb623238ef0e3411
Siddharth <siddharth99c@gmail.com>
2024-01-31 17:37:31 +0530

    feat: improves page and instructions

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 A simple way to create an RSS feed from your Framer-hosted blog.
 
 Used [lambda-rss-node](https://github.com/jwilsson/lambda-rss-node/tree/main) by [@jwilsson](https://github.com/jwilsson/) as a reference point while making this.
+
+## TODOs
+
+-   [ ] Enable better caching mechanism so fresh RSS feeds are not generated at each request
+-   [ ] Make Framer scraping logic more robust for posts
+-   [ ] Improve UI of landing page to make initial setup easier
+-   [ ] Allow self-hosting this project on Netlify/Vercel
+
+## Contributions
+
+I put this together for my own use, but any help is welcome!
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Form from "./form";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -6,21 +7,46 @@ export default function Home() {
       <header>
         <h1>Framer RSS</h1>
         <p>If you have a Framer blog, this tool can generate an RSS feed for it.</p>
-        <p>Made super late at night by <a href="https://www.twitter.com/clearlysid">@clearlysid</a>, for use on <a href="https://www.helmer.app/blog">Helmer's Blog</a>.</p>
+        <p>Made super late at night by <a href="https://www.twitter.com/clearlysid">@clearlysid</a>, for use on <a href="https://www.helmer.app/blog">Helmer</a>.</p>
       </header>
-
-      <Form />
 
       <hr />
 
       <h2>How to use</h2>
       <ol>
-        <li>Ensure you have a Framer Site, this project will not for non-Framer sites.</li>
-        <li>Add the id <code>rss</code> to the Collection List component on your blog page</li>
+        <li>Ensure you have a Framer Site, this project does not work for non-Framer sites.</li>
+        <li>In your Framer project, add the id <code>rss</code> to your <b>Collection List</b> component:
+          <div className="how-to">
+            <Image src={"/collection-list.png"} alt="Collection List component" width={200} height={130} />
+            <Image src={"/id.png"} alt="How to add an id" width={200} height={130} />
+          </div>
+        </li>
         <li>Deploy your site</li>
-        <li>Enter the URL of your site below</li>
+        <li>Enter the URL of the page containing the Collection List component</li>
         <li>Click "Create Feed"</li>
       </ol>
+
+      <Form />
+
+      <hr />
+
+      <h2>How it works</h2>
+
+      <p>This is small app that scrapes your Frame site every now and then, and generates the RSS feed to serve as needed. It is still a prototype and not recommended for production / sensitive use.</p>
+
+      <p>The code is <a href="https://github.com/clearlysid/framer-rss" target="_blank">open source</a>, in case you'd like to contribute or self-host. Consider donating via <a href="https://github.com/sponsors/clearlysid" target="_blank">Github Sponsors</a> if it helped.</p>
+
+      <hr />
+
+      <h2>Improvement Notes</h2>
+      <ol>
+        <li>Enable better caching mechanism so fresh RSS feeds are not generated at each request</li>
+        <li>Make Framer scraping logic more robust for posts</li>
+        <li>Improve UI of landing page to make initial setup easier</li>
+        <li>Allow self-hosting this project on Netlify/Vercel</li>
+      </ol>
+
+      <hr />
     </div>
   )
 }
