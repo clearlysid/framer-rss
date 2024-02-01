77dafb1c95dccd6b31ff8360e11cd400c5b08887
Siddharth <siddharth99c@gmail.com>
2024-02-01 09:17:05 +0530

    feat: updates page and readme

--- README.md
+++ README.md
@@ -1,18 +1,32 @@
 # Framer RSS
 
-A simple way to create an RSS feed from your Framer-hosted blog.
+If you have a Framer blog, this tool can generate an RSS feed for it.
 
-Used [lambda-rss-node](https://github.com/jwilsson/lambda-rss-node/tree/main) by [@jwilsson](https://github.com/jwilsson/) as a reference point while making this.
+It is small server app that scrapes your Framer site every so often, generates the feed, and serves it on a URL. It's only a prototype for now and not recommended for production / sensitive use.
+
+## Other Notes
+
+This can reliably generate RSS for most Framer sites, but there are exceptions. We need to make some assumptions about your site to be able to extract details about each post (title, date, etc.). The exact mechanism to do this varies from site to site.
+
+If it doesn't work on your site, I encourage you to open an issue on Github along with a link to it and I'll try to lend a hand.
+
+If you'd like to contribute or self-host, the code is open source. Consider donating via Github Sponsors if it helped, that way I can cover the server costs if any.
 
 [![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/clearlysid/framer-rss)
 
-## TODOs
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fclearlysid%2Fframer-rss)
+
+## Improvements
 
 -   [ ] Enable better caching mechanism so fresh RSS feeds are not generated at each request
 -   [ ] Make Framer scraping logic more robust for posts
--   [ ] Improve UI of landing page to make initial setup easier
+-   [x] Improve landing page to make initial setup easier
 -   [x] Allow self-hosting this project on Netlify/Vercel
 
 ## Contributions
 
 I put this together for my own use, but any help is welcome!
+
+## References
+
+1. [lambda-rss-node](https://github.com/jwilsson/lambda-rss-node/tree/main) by [@jwilsson](https://github.com/jwilsson/)
--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
       <header>
         <h1>Framer RSS</h1>
         <p>If you have a Framer blog, this tool can generate an RSS feed for it.</p>
+        <p>It is small server app that scrapes your Framer site every so often, generates the feed, and serves it on a URL. It's only a prototype for now and not recommended for production / sensitive use.</p>
         <p>Made super late at night by <a href="https://www.twitter.com/clearlysid">@clearlysid</a>, for use at Headout and Helmer.</p>
       </header>
 
@@ -14,15 +15,14 @@ export default function Home() {
 
       <h2>How to use</h2>
       <ol>
-        <li>Ensure you have a Framer Site, this project does not work for non-Framer sites.</li>
-        <li>In your Framer project, add the id <code>rss</code> to your <b>Collection List</b> component:
-          <div className="how-to">
+        <li>In your Framer project, add the id <code>rss</code> to your <b>Collection List</b> component.
+          <div className="grid">
             <Image src={"/collection-list.png"} alt="Collection List component" width={200} height={130} />
             <Image src={"/id.png"} alt="How to add an id" width={200} height={130} />
           </div>
         </li>
         <li>Deploy your site</li>
-        <li>Enter the URL of the page containing the Collection List component</li>
+        <li>Enter the URL of the page that has the Collection List component</li>
         <li>Click "Create Feed"</li>
       </ol>
 
@@ -30,20 +30,33 @@ export default function Home() {
 
       <hr />
 
-      <h2>How it works</h2>
+      <h2>Other Notes</h2>
 
-      <p>This is small app that scrapes your Framer site every now and then, and generates the RSS feed to serve as needed. It is still a prototype and not recommended for production / sensitive use.</p>
+      <p>This can reliably generate RSS for most Framer sites, but there are exceptions. We need to make some assumptions about your site to be able to extract details about each post (title, date, etc.). The exact mechanism to do this varies from site to site.</p>
 
-      <p>The code is <a href="https://github.com/clearlysid/framer-rss" target="_blank">open source</a>, in case you'd like to contribute or self-host. Consider donating via <a href="https://github.com/sponsors/clearlysid" target="_blank">Github Sponsors</a> if it helped.</p>
+      <p>If it doesn't work on your site, I encourage you to open an issue on Github along with a link to it and I'll try to lend a hand.</p>
+
+      <p>If you'd like to contribute or self-host, the code is <a href="https://github.com/clearlysid/framer-rss" target="_blank">open source</a>. Consider donating via <a href="https://github.com/sponsors/clearlysid" target="_blank">Github Sponsors</a> if it helped, that way I can cover the server costs if any.</p>
+
+      <div className="grid">
+        <a href="https://app.netlify.com/start/deploy?repository=https://github.com/clearlysid/framer-rss">
+          <img src="https://www.netlify.com/img/deploy/button.svg" alt="Deploy to Netlify" />
+        </a>
+        <a href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fclearlysid%2Fframer-rss">
+          <img src="https://vercel.com/button" alt="Deploy with Vercel" />
+        </a>
+
+        <iframe src="https://github.com/sponsors/clearlysid/button" title="Sponsor clearlysid" height="32" width="114" style={{ border: 0, borderRadius: "6px" }}></iframe>
+      </div>
 
       <hr />
 
-      <h2>Improvement Notes</h2>
+      <h2>Improvements</h2>
       <ol>
-        <li>Enable better caching mechanism so fresh RSS feeds are not generated at each request</li>
+        <li>Create better caching mechanism so feeds are not generated at each request</li>
         <li>Make Framer scraping logic more robust for posts</li>
-        <li>Improve UI of landing page to make initial setup easier</li>
-        <li>Allow self-hosting this project on Netlify/Vercel</li>
+        <li>✅ <s>Improve landing page to make initial setup easier</s></li>
+        <li>✅ <s>Allow self-hosting this project on Netlify/Vercel</s></li>
       </ol>
 
       <hr />
